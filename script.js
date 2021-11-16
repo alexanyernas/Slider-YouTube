95967e0a0babc8239a1165571d04ec10a12d0b43
alexanyernas <alexanyernaranjo@gmail.com>
2021-11-15 20:48:19 -0400

    Slider

---
+++ script.js
@@ -0,0 +1,18 @@
+const imgArray = [
+    './imagenes/1.jpg',
+    './imagenes/2.jpg',
+    './imagenes/3.jpg'
+]
+
+const img = document.querySelector('#slide')
+//const img = document.getElementById('slide')
+let i = 0
+
+const slideShow = () => {
+    img.src = imgArray[i]
+    i = ( i < imgArray.length - 1) ? i+1 : 0
+}
+
+const interval = () => {
+    setInterval(slideShow, 1000)
+}
